0721787c8dad595f12c9d8c1f2782d175d553ece
Amaninha <amandapnobre@gmail.com>
2019-11-22 00:51:44 -0300

    Implementing people-handler(getBirthdate and birthdateValidation).

--- people-handler.c
+++ people-handler.c
@@ -9,7 +9,7 @@ void nameValidation();
 struct Person {
     char fullName[100];
     char gender[2];
-    int birthDate[3];
+    int birthdate[3];
     char wantedCourse[100];
     char state[3];
 };
@@ -28,18 +28,27 @@ void registerPerson() {
     getGender(newPerson);
     printf("\n%s\n", newPerson->gender);
 
+    printf("\nInforme a data de nascimento:\n");
+    getBirthdate(newPerson);
+
+    printf("\nInforme o curso desejado:\n");
+    getWantedCourse(newPerson);
+
     printf("\nAperte ENTER para voltar ao menu.\n");
     fgetc(stdin);
     free(newPerson);;
 }
 
 void getName(struct Person *newPerson) {
+    int validation = 0;
     do{
         fgets(newPerson->fullName, 100, stdin);
         fflush(stdin);
 
         convertToLowercase(newPerson->fullName);
         trim(newPerson->fullName, NULL);
+
+        validation = stringIsEmpty(newPerson->fullName);
 
         if (stringIsEmpty(newPerson->fullName)){
             printf("Nome nulo, informe um nome valido.\n");
@@ -51,6 +60,7 @@ void getName(struct Person *newPerson) {
 }
 
 void getGender(struct Person *newPerson) {
+    int validation = 0;
     do{
         fgets(newPerson->gender, 50, stdin);
         fflush(stdin);
@@ -58,13 +68,15 @@ void getGender(struct Person *newPerson) {
         convertToLowercase(newPerson->gender);
         trim(newPerson->gender, NULL);
 
-        if (genderValidation(newPerson->gender)){
+        validation = genderValidation(newPerson->gender);
+
+        if (validation){
             printf("Genero invalido, informe 'M' ou 'F'.\n");
         } else {
             // Nothing to do;
         }
 
-    }while(genderValidation(newPerson->gender));
+    }while(validation);
 }
 
 int genderValidation (char *gender) {
@@ -75,4 +87,83 @@ int genderValidation (char *gender) {
     } else {
         return 0;
     }
+}
+
+void getBirthdate (struct Person *newPerson) {
+    int validation = 0;
+    do{
+        printf("Dia: ");
+        scanf("%d", &newPerson->birthdate[0]);
+        fflush(stdin);
+        printf("%d\n", newPerson->birthdate[0]);
+
+        printf("Mes: ");
+        scanf("%d", &newPerson->birthdate[1]);
+        fflush(stdin);
+        printf("%d\n", newPerson->birthdate[1]);
+
+        printf("Ano: ");
+        scanf("%d", &newPerson->birthdate[2]);
+        fflush(stdin);
+        printf("%d\n", newPerson->birthdate[2]);
+
+        validation = birthdateValitation(newPerson->birthdate);
+        if (validation){
+            printf("\nData invalida, informe uma data existente.\n");
+        } else {
+            // Nothing to do;
+        }
+
+    }while(validation);
+}
+
+int birthdateValitation (int *birthdate) {
+
+    printf("\n%d/%d/%d\n", birthdate[0], birthdate[1], birthdate[2]);
+
+    //Validate year
+    if (birthdate[2] <= 0 || birthdate[2] >= 2020){
+        printf("\n1\n");
+        return 1;
+    }
+
+    //Validate month
+    if (birthdate[1] <= 0 || birthdate[1] > 12) {
+        printf("\n2\n");
+        return 1;
+    }
+
+    //Validate months with 31 days
+    if (birthdate[1] == 1 || birthdate[1] == 3 || birthdate[1] == 5 || birthdate[1] == 7 || birthdate[1] ==  8 || birthdate[1] == 10 || birthdate[1] == 12) {
+        if (birthdate[0] <= 0 || birthdate[0] >31) {
+            printf("\n3\n");
+            return 1;
+        }
+    }
+
+    //Validate months with 30 days
+    if (birthdate[0] == 4 || birthdate[0] == 6 || birthdate[0] == 9 || birthdate[0] == 11) {
+        if (birthdate[1] <= 0 || birthdate[1] > 30) {
+            printf("\n4\n");
+            return 1;
+        }
+    }
+
+    //Validate leap year
+    if (birthdate[1] == 2) {
+        if (birthdate[2] % 4 == 0 && birthdate[2] % 100 != 0 || birthdate[2] % 400 == 0) {
+            if (birthdate[0] <= 0 || birthdate[0] > 29) {
+                printf("\n5\n");
+                return 1;
+            }
+        } else {
+            if (birthdate[0] <= 0 || birthdate[0] > 28) {
+                printf("\n6\n");
+                return 1;
+            }
+        }
+    }
+
+    //It's all ok
+    return 0;
 }
